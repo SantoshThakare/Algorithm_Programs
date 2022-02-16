4c4bf6c7713ec057f7fa843e4ef83da7a6a460e0
SantoshThakare <santoshthakare50@gmail.com>
2022-02-16 16:00:08 +0530

    Permutation

---
+++ Algorithm_program/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithm_program
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter String to find Permutation ");
+            string str = Console.ReadLine();
+            int n = str.Length;
+            Console.WriteLine(n);
+            permutation.Permutate(str, 1, n );
+
+        }
+    }
+}
